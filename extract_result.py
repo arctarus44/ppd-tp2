44d40ed87f7923db434610faa9d53be5632307d7
Dewarumez Arthur <dewarumez@a11p8.fil.univ-lille1.fr>
2015-03-11 14:29:07 +0100

    Màj extraction result.

--- extract_result.py
+++ extract_result.py
@@ -1,12 +1,14 @@
 import sys
 
+
 slice = "slice_"
 
 if __name__ == "__main__":
 	nb_slice = int(sys.argv[1])
 	result = 0
-	for i in range(1, nb_slice):
-		file = open(slice + str(i), 'r')
+	files = [f for f in os.listdir('.') if re.match(r'[0-9]+.*\.jpg', f)]
+	for f in files:
+		file = open(f)
 		result += eval(file.read())
 		file.close()
 	print(result * 4)

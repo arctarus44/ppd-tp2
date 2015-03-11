0c503949c32d011ece8085cb2211acd6400f05d6
Dewarumez Arthur <dewarumez@a11p8.fil.univ-lille1.fr>
2015-03-11 14:19:57 +0100

    Mise à jour du deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ nbMachine=`wc -l machines`
 
 deb=0
 intervalle=2000
-fin= intervalle
+fin=$intervalle
 
 for m in $machines; do
     echo "Execution sur $m .....";
-    oarsh $m python pi.py deb fin m
-    deb=fin
-    fin=deb+intervalle
+    oarsh $m python pi.py $deb $fin $m
+    deb=$fin
+    fin=$(($deb + $intervalle))
     done

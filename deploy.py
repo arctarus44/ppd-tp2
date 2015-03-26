6c1bc21bacd67289224f55f58762822ad2b75608
Arthur Dewarumez <adewarumez@lille.grid5000.fr>
2015-03-26 19:32:05 +0100

    Plein de corrections.

--- deploy.py
+++ deploy.py
@@ -6,11 +6,11 @@ BORNE_MAX = 20000
 MACHINES_CMD = "cat $OAR_FILE_NODES | uniq"
 NB_MACHINES_CMD = "cat $OAR_FILE_NODES | uniq | wc -l"
 
-SCRIPT_PI = "pi.py"
+SCRIPT_PI = "ppd-tp2/pi.py"
 
 if __name__ == "__main__":
 	machines = os.popen(MACHINES_CMD).read()
-	nb_machines = str(os.popen(NB_MACHINES_CMD).read())
+	nb_machines = int(str(os.popen(NB_MACHINES_CMD).read()))
 
 	machines = re.split("\n", machines)
 
@@ -19,16 +19,27 @@ if __name__ == "__main__":
 	borne_min = 0
 	borne_max = intervalle
 
-	p = []
-	for i in range(0, nb_machines):
-		p[i] = subp.Popen(["oarsh", machines[i], SCRIPT_PI, i, borne_min, borne_max])
+	p = [None]*(nb_machines+1)
 
+	i = 0
+	for m in machines[:-1]:
+ 		p[i] = subp.Popen(["oarsh", m, "python", SCRIPT_PI, str(i), str(borne_min), str(borne_max)])
+		i+=1
+		borne_min=borne_max
+		borne_max=borne_max+intervalle
+	
 	for i in range(0, nb_machines):
-		p[i].terminate()
+		p[i].wait()
 
-	files = [f for f in os.listdir('.') if re.match(r'slice_[0-9]*', f)]
+	files = [f for f in os.listdir('~/') if re.match(r'slice_[0-9]*', f)]
+	result = 0
 	for f in files:
+		print f
 		file = open(f)
 		result += eval(file.read())
 		file.close()
-	print(result * 4)
+
+	resultf = open("result.txt", 'w')
+	resultf.write(str(result * 4))
+	resultf.close()
+
--- pi.py
+++ pi.py
@@ -2,16 +2,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-	if len(sys.argv) == 1:		# Ici commence le master
-		# reserver les ressources et lancer le script
-		pass
-	else:
-		slice = sys.argv[1]
-		start = int(sys.argv[2])
-		end = int(sys.argv[3])
-		res = 0
-		for k in range(start, end):
-			res += ((-1) ** k)/(2 * k + 1)
-		f = open(os.path.join(os.getcwd(), "slice_" + slice)  , 'w')
-		f.write(str(res))
-		f.close()
+	task = sys.argv[1]
+	start = int(sys.argv[2])
+	end = int(sys.argv[3])
+	res = 0
+	for k in range(start, end):
+		res += (pow((-1), k))/((2 * k + 1)+0.0)
+	#f = open(os.path.join(os.getcwd(), "slice_" + task)  , 'w')
+	f = open("~/ppd-tp2/slice_" + task , 'w')
+	f.write(str(res))
+	f.close()

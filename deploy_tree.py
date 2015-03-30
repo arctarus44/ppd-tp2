2dde8a7ea27f09b05a9244d1bf48d3893de5b28b
Arthur Dewarumez <adewarumez@frennes>
2015-03-30 23:09:08 +0200

    Fin du tp

--- deploy_tree.py
+++ deploy_tree.py
@@ -14,7 +14,6 @@ def extract_result():
 	files = [f for f in os.listdir('/home/adewarumez/ppd-tp2/') if re.match(r'slice_[0-9]*', f)]
 	result = 0
 	for f in files:
-		print "tache 0 : ouverture du fichier " + f
 		slicefile = open(f)
 		result += eval(slicefile.read())
 		slicefile.close()
@@ -27,7 +26,7 @@ if __name__ == "__main__":
 
 	# Recuperation de la tache courante
 	task = int(sys.argv[1])
-	print "tache " + str(task) + " est dans la place."
+	print "tache " + str(task) + " : est dans la place."
 
 	# Recuperation de la liste des machines
 	machines = None
@@ -38,7 +37,6 @@ if __name__ == "__main__":
 		nb_machines = int(str(os.popen(NB_MACHINES_CMD).read()))
 		machine_file = open("/home/adewarumez/ppd-tp2/machines.txt", 'w')
 		for m in machines:
-			print "write " + m
 			machine_file.write(m + "\n")
 		machine_file.close()
 	else:
@@ -46,19 +44,10 @@ if __name__ == "__main__":
 		data = machine_file.read()
 		machines = re.split("\n", data)[:-2]
 		nb_machines = len(machines)
-		print "nb_machines = " + str(nb_machines)
-		print "machine lu fichier " + str(machines)
 
 	intervalle = int(BORNE_MAX / nb_machines)
 
-	# Lancement de la tache local
 	p = [None]*(3)
-	borne_min = task * intervalle
-	borne_max = borne_min + intervalle
-	print "tache " + str(task) + " : calcul pi."
-	print "tache " + str(task) + " : oarsh", machines[task], "\"python " + SCRIPT_PI + " "+ str(task) + " " + str(borne_min) + " " + str(borne_max) + "\""
-	p[0] = subp.Popen(["oarsh", machines[task], "python " + SCRIPT_PI + " "+ str(task) + " " + str(borne_min) + " " + str(borne_max)])
-
 	# Determination des taches a lancer
 	task1 = task * 2 + 1
 	task2 = task1 + 1
@@ -68,18 +57,21 @@ if __name__ == "__main__":
 		borne_min = task1 * intervalle
 		borne_max = borne_min + intervalle
 		print "tache " + str(task) + " : deploy task " + str(task1) + "."
-		print "tache " + str(task) + " : oarsh" + machines[task1] + "\"python " + SCRIPT_DEPLOY + " "+ str(task1)+"\""
 		p[1] = subp.Popen(["oarsh", machines[task1], "python " + SCRIPT_DEPLOY + " "+ str(task1)])
 
 	if task2 <= len(machines) - 1:
 		borne_min = task1 * intervalle
 		borne_max = borne_min + intervalle
 		print "tache " + str(task) + " : deploy task " + str(task2) + "."
-		print "tache " + str(task) + " : oarsh" + machines[task2] + "\"python " + SCRIPT_DEPLOY + " "+ str(task2)+"\""
 		p[2] = subp.Popen(["oarsh", machines[task2], "python " + SCRIPT_DEPLOY + " "+ str(task2)])
 
-	# Attente des taches locales et des sous taches
-	print "tache " + str(task) + " : attente resultats."
+	# Lancement de la tache local
+	borne_min = task * intervalle
+	borne_max = borne_min + intervalle
+	print "tache " + str(task) + " : calcul pi."
+	p[0] = subp.Popen(["oarsh", machines[task], "python " + SCRIPT_PI + " "+ str(task) + " " + str(borne_min) + " " + str(borne_max)])
+
+	# Attente des taches locales et des sous tache
 	for i in range(0, 3):
 		if p[i] != None:
 			p[i].wait()
